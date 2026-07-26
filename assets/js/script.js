ca0a79e58f6dcad84587ad32a2786469f03c9b6f
Christopher Phoenix <hello@chrisphoenix.dev>
2026-07-26 14:11:32 -0700

    feat: parametrically generate a grid on the canvas
    Add CSS rules to create a grid out of square divs. Add function
    that creates a grid of square divs with specified parameters.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 console.log('hello, world!');
+
+function createGrid(parent, squaresPerSide = 16, gap = '2px') {
+  const numDivs = squaresPerSide ** 2;
+
+  // set the CSS variables used to calculate the grid spacing
+  document.documentElement.style.setProperty('--grid-width', squaresPerSide);
+  document.documentElement.style.setProperty('--grid-gap', gap);
+
+  for (let i = 0; i < numDivs; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    parent.appendChild(div);
+  }
+}
+
+
+// create divs that CSS styles will arrange into a grid
+
+const container = document.querySelector(".container");
+
+createGrid(container, 12, '8px');
